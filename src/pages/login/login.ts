758b6a5f77b2d854341068a3a7cae51a3f73af5e
yzhang <yzhang@bol.com>
2017-08-17 19:58:58 +0200

    Nan | enable login right after registeration

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -24,6 +24,7 @@ export class LoginPage {
   // Our translated text strings
   private loginErrorString: string;
   showLogin: boolean = true;
+  showRegister: boolean = false;
   email: string = '';
   password: string = '';
   name: string = '';
@@ -54,45 +55,43 @@ export class LoginPage {
     //     });
     //     toast.present();
     //   });
-    if(this.showLogin) {
+    if (this.showLogin) {
       console.log('process login');
 
-      if(this.email === '' || this.password === '') {
+      if (this.email === '' || this.password === '') {
         let alert = this.alertCtrl.create({
-          title:'Register Error', 
-          subTitle:'All fields are rquired',
-          buttons:['OK']
+          title: 'Register Error',
+          subTitle: 'All fields are rquired',
+          buttons: ['OK']
         });
         alert.present();
         return;
-      }     
+      }
 
       let loader = this.loadingCtrl.create({
         content: "Logging in..."
       });
       loader.present();
-      
-      this.auth.login('basic', {'email':this.email, 'password':this.password}).then(() => {
-        console.log('ok i guess?');
+
+      this.auth.login('basic', { 'email': this.email, 'password': this.password }).then(() => {
         loader.dismissAll();
         this.alertCtrl.create({
-          title: 'success'
-        }).present();     
-        
+          title: '登陆成功'
+        }).present();
+
         this.navCtrl.push(MainPage);
 
       }, (err) => {
         loader.dismissAll();
         console.log(err.message);
-
         let errors = '';
-        if(err.message === 'UNPROCESSABLE ENTITY') errors += 'Email isn\'t valid.<br/>';
-        if(err.message === 'UNAUTHORIZED') errors += 'Password is required.<br/>';
+        if (err.message === 'UNPROCESSABLE ENTITY') errors += 'Email isn\'t valid.<br/>';
+        if (err.message === 'UNAUTHORIZED') errors += 'Password is required.<br/>';
 
         let alert = this.alertCtrl.create({
-          title:'Login Error', 
-          subTitle:errors,
-          buttons:['OK']
+          title: 'Login Error',
+          subTitle: errors,
+          buttons: ['OK']
         });
         alert.present();
       });
@@ -102,25 +101,25 @@ export class LoginPage {
   }
 
   doRegister() {
-    if(!this.showLogin) {
+    if (!this.showLogin) {
       console.log('process register');
 
       /*
       do our own initial validation
       */
-      if(this.name === '' || this.email === '' || this.password === '') {
+      if (this.name === '' || this.email === '' || this.password === '') {
         let alert = this.alertCtrl.create({
-          title:'Register Error', 
-          subTitle:'All fields are rquired',
-          buttons:['OK']
+          title: 'Register Error',
+          subTitle: 'All fields are rquired',
+          buttons: ['OK']
         });
         alert.present();
         return;
       }
 
-      let details: UserDetails = {'email':this.email, 'password':this.password, 'name':this.name};
+      let details: UserDetails = { 'email': this.email, 'password': this.password, 'name': this.name };
       console.log(details);
-      
+
       let loader = this.loadingCtrl.create({
         content: "Registering your account..."
       });
@@ -128,34 +127,45 @@ export class LoginPage {
 
       this.auth.signup(details).then(() => {
         console.log('ok signup');
-        this.auth.login('basic', {'email':details.email, 'password':details.password}).then(() => {
+        this.auth.login('basic', { 'email': details.email, 'password': details.password }).then(() => {
           loader.dismissAll();
           this.alertCtrl.create({
             title: 'success'
-          }).present();   
+          }).present();
         });
+        return this.auth.login('basic', { 'email': details.email, 'password': details.password }).then(() => {
+          this.navCtrl.push(MainPage);
 
-      }, (err:IDetailedError<string[]>) => {
+        }
+
+        );
+
+      }, (err: IDetailedError<string[]>) => {
         loader.dismissAll();
         let errors = '';
-        for(let e of err.details) {
+        for (let e of err.details) {
           console.log(e);
-          if(e === 'required_email') errors += 'Email is required.<br/>';
-          if(e === 'required_password') errors += 'Password is required.<br/>';
-          if(e === 'conflict_email') errors += 'A user with this email already exists.<br/>';
+          if (e === 'required_email') errors += 'Email is required.<br/>';
+          if (e === 'required_password') errors += 'Password is required.<br/>';
+          if (e === 'conflict_email') errors += 'A user with this email already exists.<br/>';
           //don't need to worry about conflict_username
-          if(e === 'invalid_email') errors += 'Your email address isn\'t valid.';
+          if (e === 'invalid_email') errors += 'Your email address isn\'t valid.';
         }
         let alert = this.alertCtrl.create({
-          title:'Register Error', 
-          subTitle:errors,
-          buttons:['OK']
+          title: 'Register Error',
+          subTitle: errors,
+          buttons: ['OK']
         });
         alert.present();
       });
-     
+
     } else {
       this.showLogin = false;
     }
+  }
+
+  doShowRegister() {
+    this.showRegister = true;
+    this.showLogin = false;
   }
 }
