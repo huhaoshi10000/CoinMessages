e941f8ddf698a7942617d474e76bb05aef9c5adc
yzhang <yzhang@bol.com>
2017-08-19 22:14:22 +0200

    Nan | login page improve ment

--- src/pages/welcome/welcome.ts
+++ src/pages/welcome/welcome.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-
-
+import { NavController, ToastController, LoadingController } from 'ionic-angular';
+import { Auth, User, UserDetails, IDetailedError , } from '@ionic/cloud-angular';
+import { MainPage } from '../../pages/pages';
 import { LoginPage } from '../login/login';
 import { SignupPage } from '../signup/signup';
+import { Storage } from '@ionic/storage';
 
 /**
  * The Welcome Page is a splash page that quickly describes the app,
@@ -17,10 +18,123 @@ import { SignupPage } from '../signup/signup';
 })
 export class WelcomePage {
 
-  constructor(public navCtrl: NavController) { }
+  constructor(private toastCtrl: ToastController,  private storage: Storage, public navCtrl: NavController, public auth: Auth, public user: User,  public loadingCtrl: LoadingController) { }
+  
+  private  showLogin: boolean = true;
+  showRegister: boolean = false;
+  email: string = '';
+  password: string = '';
+  name: string = '';
 
-  login() {
-    this.navCtrl.push(LoginPage);
+  
+
+  doLogin() {
+    if (this.showLogin) {
+      console.log('process login');
+
+      if (this.email === '' || this.password === '') {
+        let toast = this.toastCtrl.create({
+          message: "请填写邮箱及密码",
+          position: 'top',
+          showCloseButton: true,
+          closeButtonText: '确认'
+        });
+        toast.present();
+        return;
+      }
+
+      let loader = this.loadingCtrl.create({
+        content: "Logging in..."
+      });
+      loader.present();
+
+      this.auth.login('basic', { 'email': this.email, 'password': this.password }).then(() => {
+        loader.dismissAll();
+        this.navCtrl.push(MainPage);
+        this.storage.set('loginDetails', { 'email': this.email, 'password': this.password });
+      }, (err) => {
+        loader.dismissAll();
+        console.log(err.message);
+        let errors = '';
+        if (err.message === 'UNPROCESSABLE ENTITY') errors += 'Email isn\'t valid.<br/>';
+        if (err.message === 'UNAUTHORIZED') errors += 'Password is required.<br/>';
+
+        let toast = this.toastCtrl.create({
+          message: '登陆失败',
+          position: 'top'                 
+        });
+        toast.present();
+      });
+    } else {
+      this.showLogin = true;
+    }
+  }
+
+  doRegister() {
+    if (!this.showLogin) {
+      console.log('process register');
+
+      if (this.name === '' || this.email === '' || this.password === '') {
+        let toast = this.toastCtrl.create({
+          message: '请填写相应信息',
+          position: 'top',
+          showCloseButton: true,
+          closeButtonText: '确认'
+        });
+        toast.present();
+        return;
+      }
+
+      let details: UserDetails = { 'email': this.email, 'password': this.password, 'name': this.name };
+      console.log(details);
+
+      let loader = this.loadingCtrl.create({
+        content: "Registering your account..."
+      });
+      loader.present();
+
+      this.auth.signup(details).then(() => {
+        this.storage.set('loginDetails', { 'email': this.email, 'password': this.password });        
+        console.log('ok signup');
+        this.auth.login('basic', { 'email': details.email, 'password': details.password }).then(() => {
+          loader.dismissAll();
+          this.toastCtrl.create({
+            message: '注册成功',
+            position: 'top'
+          }).present();
+        });
+        return this.auth.login('basic', { 'email': details.email, 'password': details.password }).then(() => {
+          this.navCtrl.push(MainPage);
+
+        }
+
+        );
+
+      }, (err: IDetailedError<string[]>) => {
+        loader.dismissAll();
+        let errors = '';
+        for (let e of err.details) {
+          console.log(e);
+          if (e === 'required_email') errors += 'Email is required.<br/>';
+          if (e === 'required_password') errors += 'Password is required.<br/>';
+          if (e === 'conflict_email') errors += 'A user with this email already exists.<br/>';
+          if (e === 'invalid_email') errors += 'Your email address isn\'t valid.';
+        }
+        let toast = this.toastCtrl.create({
+          message: '注册失败',
+          position: 'top'
+        });
+        toast.present();
+      });
+
+    } else {
+      this.showLogin = false;
+    }
+  }
+
+  doShowRegister() {
+    this.showRegister = true;
+    this.showLogin = false;
   }
 
   // signup() {
